c582a1be6351b8f1a244521313742ec375afd76a
chenmuyao <chenmuyao95@gmail.com>
2018-10-01 21:26:16 +0200

    Add git simple

---
+++ git_end.sh
@@ -0,0 +1,18 @@
+echo "Please leave a message: "
+read message
+
+git add -A
+git commit -m "$message"
+
+echo "Push or not?(y/n)"
+
+read push
+
+if [$push == "y" || $push == "Y"] ;
+then 
+    echo "To which branch?"
+    read branch
+else exit
+fi
+
+git push origin $branch
