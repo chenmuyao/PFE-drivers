ac0f3f9cf1d38807eecc5fe7e38af6dbe67a0ab2
chenmuyao <chenmuyao95@gmail.com>
2018-10-27 23:13:38 +0200

    led driver

---
+++ Drivers/led_drv/.tmp_versions/led_drv.mod
@@ -0,0 +1,2 @@
+/home/chenmy/ARMLINUX/PFE-drivers/Drivers/led_drv/led_drv.ko
+/home/chenmy/ARMLINUX/PFE-drivers/Drivers/led_drv/led_drv.o
--- Drivers/led_drv/led_drv.c
+++ Drivers/led_drv/led_drv.c
@@ -1,18 +1,28 @@
 #include <linux/module.h>
-
+#include <linux/kernel.h>
+#include <linux/gpio.h>
+#include <asm/uaccess.h>
+#include <linux/fs.h>
+#include <linux/init.h>
 //#error Are we building this file?
 
+#define LED1	49 
+
 static struct class 				*leddrv_class;
-static struct class_device *leddrv_class_dev;
+static struct device *leddrv_class_dev;
 
 static int led_drv_open(struct inode *inode, struct file *file)
 {
-	//TODO
+	gpio_direction_output(LED1, 0);
+	return 0;
 }
 
 static ssize_t led_drv_write(struct file *file, const char __user *buf, size_t count, loff_t * ppos)
 {
-	//TODO
+	int val;
+	copy_from_user(&val, buf, count);
+	gpio_set_value(LED1, val);
+	return 0;
 }
 
 static struct file_operations led_drv_fops = {
@@ -27,17 +37,19 @@ static int __init led_drv_init(void)
 	printk(KERN_INFO "----> Led driver init()\n");
 	major = register_chrdev(0,"led_drv", &led_drv_fops);
 	leddrv_class = class_create(THIS_MODULE, "leddrv"); // ??
-	leddrv_class_dev = class_device_create(leddrv_class, NULL , MKDEV(major,0), NULL, "xyz");
-	// TODO
+	leddrv_class_dev = device_create(leddrv_class, NULL , MKDEV(major,0), NULL, "xyz");
+	gpio_request(LED1, "sysfs");
+	gpio_export(LED1, false);
 	return 0;
 }
 
 static void __exit led_drv_exit(void)
 {
 	unregister_chrdev(major,"led_drv");
-	class_device_unregister(leddrv_class_dev);
+	device_unregister(leddrv_class_dev);
 	class_destroy(leddrv_class);
-	//TODO
+
+	gpio_free(LED1);
 	printk(KERN_INFO "<---- Led driver exit().\n");
 }
 
