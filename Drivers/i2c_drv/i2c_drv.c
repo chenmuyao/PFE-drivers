745eb59f2d32f4c906366a2e75c9635263f6c746
chenmuyao <chenmuyao95@gmail.com>
2019-01-08 20:53:40 +0100

    i2cOK

--- Drivers/i2c_drv/i2c_drv.c
+++ Drivers/i2c_drv/i2c_drv.c
@@ -18,7 +18,7 @@ static struct i2c_client *at24cxx_client;
  */
 static ssize_t at24cxx_read(struct file * file, char __user *buf, size_t count, loff_t *off)
 {
-	unsigned char addr, data;
+	unsigned char addr, data = 0xBB;
 	
 	copy_from_user(&addr, buf, 1);
 	data = i2c_smbus_read_byte_data(at24cxx_client, addr);
@@ -32,16 +32,51 @@ static ssize_t at24cxx_read(struct file * file, char __user *buf, size_t count, 
 static ssize_t at24cxx_write(struct file *file, const char __user *buf, size_t count, loff_t *off)
 {
 	unsigned char ker_buf[2];
-	unsigned char addr, data;
-
+	unsigned char addr;
+	unsigned char data;
+	//unsigned char data[1];
+	unsigned int err;
+/*
+		struct i2c_msg msg[1] = {
+			{
+				.addr = 0,
+				.flags = at24cxx_client->flags,
+				.len = 1,
+				.buf = data,
+			},
+		};
+*/
 	copy_from_user(ker_buf, buf, 2);
 	addr = ker_buf[0];
 	data = ker_buf[1];
+	//data[0] = ker_buf[1];
 
+	//msg[0].addr = addr;
+
+	//printk("addr = 0x%02x, data = 0x%02x\n", addr, data[0]);
 	printk("addr = 0x%02x, data = 0x%02x\n", addr, data);
+	/*
+	  * Warning: Can't use SMBus Quick Write command, will skip some addresses
+     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+00:                                                 
+10:                                                 
+20:                                                 
+30: -- -- -- -- -- -- -- --                         
+40:                                                 
+50: UU -- -- -- UU UU UU UU -- -- -- -- -- -- -- -- 
+60:                                                 
+70:                                                 
+	  */
+	err =	i2c_smbus_write_byte_data(at24cxx_client, addr, data); // System Management Bus
+	printk("write error %d\n", err);
 
-	if (!i2c_smbus_write_byte_data(at24cxx_client, addr, data))
+	//err = i2c_transfer(at24cxx_client->adapter, msg, 1);
+	//printk("write error %d\n", err);
+	if (!err) 
+	{	
+		printk("write error %d\n", err);
 		return 2;
+	}
 	else
 		return -EIO;	
 }
--- Drivers/i2c_drv/i2c_drv.mod.c
+++ Drivers/i2c_drv/i2c_drv.mod.c
@@ -21,14 +21,14 @@ __attribute__((section("__versions"))) = {
 	{ 0xd92d6772, "i2c_del_driver" },
 	{ 0x2e5810c6, "__aeabi_unwind_cpp_pr1" },
 	{ 0x9d524220, "i2c_register_driver" },
+	{ 0x8a2cfdc, "i2c_smbus_write_byte_data" },
+	{ 0x27e1a049, "printk" },
 	{ 0x6c4fc9f7, "device_create" },
 	{ 0x95d52825, "__class_create" },
 	{ 0xcd7317e5, "__register_chrdev" },
+	{ 0xfa2a45e, "__memzero" },
 	{ 0x67c2fa54, "__copy_to_user" },
 	{ 0xc6085144, "i2c_smbus_read_byte_data" },
-	{ 0xfa2a45e, "__memzero" },
-	{ 0x8a2cfdc, "i2c_smbus_write_byte_data" },
-	{ 0x27e1a049, "printk" },
 	{ 0xfbc74f64, "__copy_from_user" },
 	{ 0xefd6cf06, "__aeabi_unwind_cpp_pr0" },
 	{ 0x6bc3fbc0, "__unregister_chrdev" },
@@ -42,4 +42,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "F16B896CC693897DF548570");
+MODULE_INFO(srcversion, "8BC7D4EABFC6C8AB039FD5B");
